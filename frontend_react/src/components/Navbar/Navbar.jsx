0502a909f77a9507ef7888c174402c169a9a4a90
Juanjo Guirao <juan.jose.guirao@gmail.com>
2022-05-17 18:07:12 +0100

    Finished works section

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false);
     return (
         <nav className='app__navbar'>
-            <di className='app__navbar-logo'>
+            <div className='app__navbar-logo'>
                 <img src={images.logo} alt='logo' />
-            </di>
+            </div>
             <ul className='app__navbar-links'>
                 {["home", "about", "work", "skills", "contact"].map(item => (
                     <li className='app__flex p-text' key={`link-${item}`}>
